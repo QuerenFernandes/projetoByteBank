8d850302d2163134f536ce41108a09ac2ac28197
Queren Hapuque <queren.fernandes@yahoo.com>
2023-03-22 19:39:03 -0300

    alterando layout

---
+++ js/dark-mode.js
@@ -0,0 +1,9 @@
+document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#sidebar").classList.toggle("dark-mode-sidebar");
+    document.querySelector("#main").classList.toggle("dark-mode");
+    document.querySelector("#button").classList.toggle("mudar-icone");
+    document.querySelector("#cabecalho").classList.toggle("dark-cabecalho");
+    document.querySelector("#balao").classList.toggle("dark-cabecalho");
+    document.querySelector("#rodape").classList.toggle("dark-titulo")
+
+})
